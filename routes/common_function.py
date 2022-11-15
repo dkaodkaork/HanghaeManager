ccc14cc2f1138bc63a3e3ec98ed10bcafcc8d646
mina <m1nna@naver.com>
2022-11-15 22:40:36 +0900

    feat: 오늘 날짜 받아 오는 함수

--- routes/common_function.py
+++ routes/common_function.py
@@ -1,12 +1,12 @@
 import datetime
 
 
-# 파라미터 date_format값은 'time' 과 'sametime'만 을 가지고 사용하면 좋을 것 같아욥!
+# 파라미터 date_format값은 'othertime' 과 'sametime'만 을 가지고 사용하면 좋을 것 같아욥!
 def now_time(date_format):
     now = datetime.datetime.now()
 
     # 시간까지 저장하고 싶은 경우
-    if(date_format == 'time'):
+    if(date_format == 'othertime'):
         return now
     else: # 시간 관계 없이 날짜만 영향 받고 싶은 경우 년-월-일 00:00:00 포맷
         date_format = '%Y-%m-%d'
