79455e0e570d805672813690555fcc40939d889a
mina <m1nna@naver.com>
2022-11-17 10:42:24 +0900

    fix: 유저 아이디 받아오는 로직 추가

--- routes/question.py
+++ routes/question.py
@@ -3,14 +3,18 @@ from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
 import certifi
+from db import db
+
+import routes.login as jwt_check
+import routes.common_function as common_function
 
 question = Blueprint("question", __name__, url_prefix="/question")
 
-load_dotenv()
-DB = os.getenv('DB')
-client = MongoClient(DB, tlsCAFile=certifi.where())
-
-db = client.testQuestions
+# load_dotenv()
+# DB = os.getenv('DB')
+# client = MongoClient(DB, tlsCAFile=certifi.where())
+#
+# db = client.testQuestions
 
 @question.route('/')
 def question_home():
@@ -18,20 +22,39 @@ def question_home():
 
 @question.route("/question", methods=["POST"])
 def question_post():
-    question_title_receive = request.form['question_title_give']
-    question_detail_receive = request.form['question_detail_give']
-    main_ability_receive = request.form['main_ability_give']
-    
-    question_list = list(db.testQuestions.find({},{'_id':False}))
-    count = len(question_list) +1
-    
-    doc = {
-        'question_title': question_title_receive,
-        'question_detail': question_detail_receive,
-        'main_ability': main_ability_receive,
-        'question_id': count
-    }
-    
-    db.testQuestions.insert_one(doc)
-    return 
 
+    user_check = jwt_check.user_check()
+
+    if user_check['result'] != "fail":
+        user_id = user_check['user_id']
+
+        user = db.users.find_one({'user_id': user_id}, {'_id': False})
+        user_name = user['user_name']
+
+        question_title_receive = request.form['question_title_give']
+        question_detail_receive = request.form['question_detail_give']
+        main_ability_receive = request.form['main_ability_give']
+
+        question_list = list(db.testQuestions.find({},{'_id': False}))
+        count = len(question_list) +1
+
+        question_date = common_function.now_time('othertime')
+
+
+        doc = {
+            'user_id': user_id,
+            'user_name': user_name,
+            'question_title': question_title_receive,
+            'question_detail': question_detail_receive,
+            'main_ability': main_ability_receive,
+            'question_id': count,
+            'question_date': question_date,
+            'answer_list': []
+        }
+
+        db.question.insert_one(doc)
+        return jsonify({'message': "게시글 등록 완료! "})
+
+    else:
+
+        return jsonify({'message': "다시 시도해주세요. "})
